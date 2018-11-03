829227dae43c6061fc2a69c6bed7c2597654d1ca
Aisling O'Mahony <31589307+aislingomahony@users.noreply.github.com>
2018-11-03 18:21:35 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+//
+//Blackjack 
+//by Aisling O'Mahony
+//
+
+let card1 = "Ace of Diamonds", card2 = "Ten of Clubs";
+let minBet = 5.00;
+
+console.log("Welcome to Blackjack!");
+
+console.log("You are dealt: ");
+console.log("  " + card1);
+console.log("  " + card2);
+console.log("Your minimum bet is: " + "€" + minBet);
